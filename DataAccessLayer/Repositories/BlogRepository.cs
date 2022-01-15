889cc55a6db4bb645b0af85c24c888ed0520adfb
Muhammed <mhmmd.dncr1414@gmail.com>
2022-01-15 23:21:49 +0300

    Add Template

--- DataAccessLayer/Repositories/BlogRepository.cs
+++ DataAccessLayer/Repositories/BlogRepository.cs
@@ -20,6 +20,11 @@ namespace DataAccessLayer.Repositories
             }
         }
 
+        public void Delete(Blog t)
+        {
+            throw new NotImplementedException();
+        }
+
         public void DeleteBlog(Blog blog)
         {
             using (var c = new Context())
@@ -37,12 +42,32 @@ namespace DataAccessLayer.Repositories
             }
         }
 
+        public Blog GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Blog> GetListAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Blog t)
+        {
+            throw new NotImplementedException();
+        }
+
         public List<Blog> ListAllBlog()
         {
             using (var c = new Context())
             {
                 return c.Blogs.ToList();
             }
+        }
+
+        public void Update(Blog t)
+        {
+            throw new NotImplementedException();
         }
 
         public void UpdateBlog(Blog blog)
