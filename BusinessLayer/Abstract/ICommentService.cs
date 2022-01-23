fd17487664c0bc5a2b819d61f188c9672905278d
Muhammed <mhmmd.dncr1414@gmail.com>
2022-01-23 21:05:19 +0300

    Add View Components
    And Example..

---
+++ BusinessLayer/Abstract/ICommentService.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface ICommentService
+    {
+        void AddComment(Comment comment);
+       //  void UpdateComment(Category category);
+       // void DeleteCategory(Category category);
+        List<Comment> GetList(int id);
+       // Category GetById(int id);
+    }
+}
---
+++ CoreDemo/ViewComponents/CommentList.cs
@@ -0,0 +1,35 @@
+﻿using CoreDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.ViewComponents
+{
+    public class CommentList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var commentValues = new List<UserComment>
+            {
+                new UserComment
+                {
+                    ID=1,
+                    UserName="DNCR"
+                },
+                new UserComment
+                {
+                    ID=2,
+                    UserName="Murat"
+                },
+                new UserComment
+                {
+                    ID=3,
+                    UserName="Ayşe"
+                }
+            };
+            return View(commentValues);
+        }
+    }
+}
