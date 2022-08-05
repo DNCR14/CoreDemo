8dd5c1240392f529cdcf7a1a00b188bf80b1954f
Muhammed <mhmmd.dncr1414@gmail.com>
2022-08-06 01:35:23 +0300

    42. Video

---
+++ CoreDemo/Controllers/ContactController.cs
@@ -0,0 +1,28 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+    public class ContactController : Controller
+    {
+        ContactManager cm = new ContactManager(new EfContactRepository());
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Contact contact)
+        {
+            contact.ContactDate = System.DateTime.Now;
+            contact.ContactStatus = true;
+            cm.ContactAdd(contact);
+
+            return RedirectToAction("Index", "Blog");
+        }
+    }
+}
