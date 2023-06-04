5d4b5b22c99621a51d221433eb53f045c5f2b36c
Muhammed <mhmmd.dncr1414@gmail.com>
2023-06-04 16:37:17 +0300

    #97

--- CoreDemo/Areas/Admin/Controllers/BlogController.cs
+++ CoreDemo/Areas/Admin/Controllers/BlogController.cs
@@ -1,9 +1,11 @@
 ﻿using ClosedXML.Excel;
 using CoreDemo.Areas.Admin.Models;
+using DataAccessLayer.Concrete;
 using DocumentFormat.OpenXml.Office2010.ExcelAc;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace CoreDemo.Areas.Admin.Controllers
 {
@@ -61,5 +63,44 @@ namespace CoreDemo.Areas.Admin.Controllers
         {
             return View();
         }
+
+        public IActionResult ExportDinamikExcelBlogList()
+        {
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Blog Listesi");
+                worksheet.Cell(1, 1).Value = "Blog ID";
+                worksheet.Cell(1, 2).Value = "Blog Adı";
+
+                int BlogRowCount = 2;
+                foreach (var item in GetBlogList2())
+                {
+                    worksheet.Cell(BlogRowCount, 1).Value = item.ID;
+                    worksheet.Cell(BlogRowCount, 2).Value = item.BlogName;
+                    BlogRowCount++;
+                }
+                using (var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Excel.xlsx");
+                }
+            }
+        }
+        public List<BlogModel> GetBlogList2()
+        {
+            List<BlogModel> blogList = new List<BlogModel>();
+            using(var c = new Context())
+            {
+                blogList = c.Blogs.Select(x => new BlogModel
+                {
+                    ID = x.BlogID,
+                    BlogName = x.BlogTitle
+                }).ToList();
+            }
+            return blogList;
+
+        }
+
     }
 }
