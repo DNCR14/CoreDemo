57f557aec37d138c55c288037e3b1597fb999c37
Muhammed <mhmmd.dncr1414@gmail.com>
2023-06-18 21:27:25 +0300

    #105

---
+++ CoreDemo/Areas/Admin/Controllers/ChartController.cs
@@ -0,0 +1,39 @@
+﻿using CoreDemo.Areas.Admin.Models;
+using DocumentFormat.OpenXml.Office2010.ExcelAc;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CoreDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ChartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CategoryChart()
+        {
+            List<CategoryClass> list = new List<CategoryClass>();
+
+            list.Add(new CategoryClass
+            {
+                categoryname = "Teknoloji",
+                categorycount = 10
+            });
+            list.Add(new CategoryClass
+            {
+                categoryname = "Blog",
+                categorycount = 7
+            });
+            list.Add(new CategoryClass
+            {
+                categoryname = "Sinema",
+                categorycount = 1
+            });
+
+            return Json(new { jsonlist = list });
+        }
+    }
+}
--- CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
+++ CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -4,6 +4,7 @@ using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
 using SixLabors.Fonts;
 using System.Linq;
+using System.Xml.Linq;
 
 namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
 {
@@ -16,6 +17,10 @@ namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
             ViewBag.v1 = bm.GetList().Count();
             ViewBag.v2 = c.Contacts.Count();
             ViewBag.v3 = c.Comments.Count();
+            string apiKey = "9f11e966dbb7d030bb2438e6d4843c3e";
+            string connection = "https://api.openweathermap.org/data/2.5/weather?q=Ankara&appid="+apiKey+"&units=metric&mode=xml";
+            XDocument xDocument = XDocument.Load(connection);
+            ViewBag.v4 = xDocument.Descendants("temperature").ElementAt(0).Attribute("value").Value;
             return View();
         }
     }
